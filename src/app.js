2925539d0733f3dd83dc62369db330247c381f5e
John Doe <webdevnorth@gmail.com>
2016-06-29 13:43:22 +0100

    removed mongo dependecies, restructured app.js boilerplate

--- src/app.js
+++ src/app.js
@@ -2,24 +2,10 @@
 var express = require('express');
 var multer = require('multer');
 
-// MongoDB variables
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
-var mongoUrl = 'mongodb://localhost:27017/file_meta';
-var db;
-var fileCollection;
-
-
 var app = express();
-// Initialize connection once
-// Reuse database object in request handlers
-MongoClient.connect(mongoUrl, function(err, database) {
-  if(err) throw err;
 
-  db = database;
-  fileCollection = db.collection('file_meta');
-
-  // Start the application after the database connection is ready
-  app.listen(3000);
-  console.log("Listening on port 3000");
+app.get('/', function (req, res) {
+	res.send('Root.');
 });
+
+app.listen('8000', console.log('Server is running...'));
