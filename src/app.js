70381785160414fddcd06aeaa86161dd6d1163dd
John Doe <webdevnorth@gmail.com>
2016-07-01 16:27:59 +0100

    html near-completion, app.js updated, port error fixed

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ var express = require('express');
 var multer = require('multer');
 
 var app = express();
+var upload = multer();
 
-app.get('/', function (req, res) {
-	res.send('Root.');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/index.html');  
+}); // end '/'
+
+app.post('/', upload.single('metaFile'), function (req, res){	
+	// size 	Size of the file in bytes
+    console.log(req.body) // form fields
+    console.log(req.files) // form files
+    res.status(204).end()
 });
 
-app.listen('8000', console.log('Server is running...'));
+app.listen(3000, console.log('Server is running...'));
